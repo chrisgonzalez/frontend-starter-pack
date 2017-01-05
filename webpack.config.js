905b20d7688d77d366b94dc9f197121ec95f69d9
Chris Gonzalez <chris.gonzalez@turner.com>
2017-01-05 12:09:48 -0500

    Updating to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
     entry: {
         main: [
             // polyfills
             'whatwg-fetch',
-            'es5-shim',
+            // 'es5-shim',
             'es6-promise',
             path.join(__dirname, 'js/main.js')
         ]
     },
 
     output: {
-        path: __dirname,
-        filename: 'js/[name].bundle.js',
+        path: path.join(__dirname, prod ? '/dist/' : ''),
+        filename: '/js/[name].bundle.js',
         /* path root for imports */
         publicPath: __dirname
     },
 
     module: {
-        preLoaders: [
-            {
-                test: /\.json$/,
-                loader: 'json'
-            }
-        ],
-
         loaders: [
             {
                 test: /\.js$/,
@@ -42,15 +36,13 @@ module.exports = {
     },
 
     resolve: {
-        root: __dirname,
+        modules: [__dirname, path.join(__dirname, '/node_modules')],
         extensions: [
-            '',
             '.js'
         ]
     },
 
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(true),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     ]
 };
